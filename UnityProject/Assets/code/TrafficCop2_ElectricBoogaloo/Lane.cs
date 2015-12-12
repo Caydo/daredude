c7e7aeb3611a3814a7eff0e996496bf2e7650507
Scottya21 <Scottya21@gmail.com>
2015-12-12 08:36:26 -0700

    make cars queue nicely at stopped lights

--- UnityProject/Assets/code/TrafficCop2_ElectricBoogaloo/Lane.cs
+++ UnityProject/Assets/code/TrafficCop2_ElectricBoogaloo/Lane.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public class Lane : MonoBehaviour {
     public bool allowEntry; // whether cars can enter this lane
     public int indexInParent; // lane indices go from left to right in an anti-clockwise direction.
-    public float carRangeMin = -.4f;
-    public float carRangeMax = .4f;
-    public float successRange = 6f;
+    public float carRangeMin = -4.6f;
+    public float carRangeMax = -2.5f;
+    public float successRange = 4.6f;
 }
--- UnityProject/Assets/code/TrafficCop2_ElectricBoogaloo/States/EnterQueueState.cs
+++ UnityProject/Assets/code/TrafficCop2_ElectricBoogaloo/States/EnterQueueState.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Linq;
 
 public class EnterQueueState : GoingState {
-    public float CAR_SEPARATION = .05f;
+    public float CAR_SEPARATION = 1f;
     Vehicle self;
     Lane selfLane; 
     StateMachine stateMachine; 
@@ -20,8 +20,13 @@ public class EnterQueueState : GoingState {
 
         var currentPosition = self.transform.position;
         var vehiclesAhead = self.transform.parent.Cast<Transform>()
-            .Where(t => ((directionPosition(currentPosition) - directionPosition(t.position)).sqrMagnitude < 0)
-                && (t.GetComponent<Vehicle>() != null))
+            .Where(t => {
+                var notSelf = t!= self.transform;
+                var isAhead = directionMagnitude(t.position) > directionMagnitude(self.transform.position);
+                var isVehicle = (t.GetComponent<Vehicle>() != null);
+                //Debug.Log(string.Format("checking vehicles ahead {0} {1} {2} {3} {4}", self.transform.name, t.name, notSelf, isAhead, isVehicle));
+                return notSelf && isAhead && isVehicle;
+            })
             .Select(t => t.position)
             .Select(t => directionMagnitude(t))
             .OrderBy(t => t);
@@ -29,7 +34,15 @@ public class EnterQueueState : GoingState {
         var separation = selfLane.carRangeMax;
         if(vehiclesAhead.Any()) 
         {
-            separation = Mathf.Max(0, vehiclesAhead.First() - directionMagnitude(currentPosition) - CAR_SEPARATION);
+            separation = vehiclesAhead.First() - directionMagnitude(currentPosition);
+
+            //Debug.Log(string.Format("checking vehicles ahead 2 {0} {1} {2} {3} {4}", self.transform.name, vehiclesAhead.First(), directionMagnitude(currentPosition), separation, CAR_SEPARATION));
+            if (separation <= CAR_SEPARATION)
+            {
+                // if we caught up to another car, then we need to lose the velocity we've gained 
+                self.velocity = 0;
+                separation = directionMagnitude(currentPosition);
+            }
         }
 
         var newPosDueToVelocity = (directionMagnitude(currentPosition) + (self.velocity * Time.deltaTime));
@@ -38,15 +51,10 @@ public class EnterQueueState : GoingState {
         self.transform.position = self.transform.position + (scaleOfNewPosition * Vehicle.FORWARD_DIRECTION);
 
         // if we hit the end of the lane wait for it to be 'go' status
-        //Debug.Log(string.Format("checking range {0} {1}", self.transform.position, selfLane.carRangeMax));
+        //Debug.Log(string.Format("checking range {0} {1} {2} {3}", self.transform.position, selfLane.carRangeMax, self.transform.name, vehiclesAhead.Any()));
         if (directionMagnitude(self.transform.position) >= selfLane.carRangeMax)
         {
             stateMachine.GoTo("WaitToGo");
-        }
-        // if we hit another car, then we need to lose acceleration
-        else if(vehiclesAhead.Any() && directionMagnitude(self.transform.position) >= (vehiclesAhead.First() - CAR_SEPARATION))
-        {
-            self.acceleration = 0;
         }
 	}
 
