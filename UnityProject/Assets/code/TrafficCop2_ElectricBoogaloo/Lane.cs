09b4a5ebba0b55b4d6440825115c4fb55a595517
Scottya21 <Scottya21@gmail.com>
2015-12-11 23:01:37 -0700

    Setting up stuff for traffic scene

---
+++ UnityProject/Assets/code/TrafficCop2_ElectricBoogaloo/Lane.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum TrafficPolicy
+{
+    Stop=0, Go=1//, Yield?
+}
+public class Lane : MonoBehaviour {
+    public TrafficPolicy currentPolicy; 
+    public bool allowEntry; // whether cars can enter this lane
+    public int indexInParent; // lane indices go from left to right in an anti-clockwise direction.
+}
---
+++ UnityProject/Assets/code/TrafficCop2_ElectricBoogaloo/TrafficDirector.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class TrafficDirector : MonoBehaviour {
+    public Transform roads;
+    public int currentRoad; // road cop is currently directing
+    public Transform CurrentRoad()
+    {
+        return roads.GetChild(currentRoad);
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonUp("Fire1")) // fire1 will be the 'rotate' button; 
+            // TODO I think that we probably want it to keep rotating if held down but not too fast 
+        {
+            int maxIndex = roads.childCount;
+            currentRoad = (currentRoad + 1 >= maxIndex) ? 0 : (currentRoad + 1);
+        }
+
+        if(Input.GetButtonUp("Fire2")) // fire 2 will be the toggle road type 
+        {
+            foreach(Lane l in CurrentRoad().Cast<Transform>()
+                .Select(t => t.GetComponent<Lane>())
+                .Where(t => t != null && !t.allowEntry)) // only lanes that allow egress need a stop/go status 
+            {
+                l.currentPolicy = (l.currentPolicy == TrafficPolicy.Go) ? TrafficPolicy.Stop : TrafficPolicy.Go;
+            }
+        }
+    }
+}
