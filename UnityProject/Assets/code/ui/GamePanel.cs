e720ae7b44a8b8eb27e5ad605fce95886cac103a
Recursiveanomaly <marnelltom@gmail.com>
2015-12-12 09:07:11 -0700

    Setting up displaying Judged Person to view.

---
+++ UnityProject/Assets/code/ui/GamePanel.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.code.data;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class GamePanel : MonoBehaviour
+{
+    public Image m_judgedPortrait;
+    public Text m_judgedName;
+    public List<Text> m_judgedTraits;
+    public Text m_judgedSpeechText;
+    public List<QuestionControl> m_questions;
+
+    void SetJudgedPerson(JudgedPerson person)
+    {
+
+    }
+}
