6faeb6aac4d30b29ee5013756a1d16ce7b2695b0
Recursiveanomaly <marnelltom@gmail.com>
2015-12-13 11:29:53 -0800

    Add sounds for questions.

---
+++ UnityProject/Assets/code/ui/PlayRandomAudioSource.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayRandomAudioSource : MonoBehaviour
+{
+    public List<AudioSource> m_audioSources;
+
+    public void PlayRandom()
+    {
+        if(m_audioSources != null)
+        {
+            int index = Random.Range(0, m_audioSources.Count);
+            if(m_audioSources[index] != null) m_audioSources[index].Play();
+        }
+    }
+}
