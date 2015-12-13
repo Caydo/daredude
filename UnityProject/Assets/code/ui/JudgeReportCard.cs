864efead9bb74aeba05805ba4864066d13618331
Caydo <directorjoe@gmail.com>
2015-12-13 13:43:41 -0700

    Fixing absolved/damned souls count display. Disabling questions as well as absolve/damn buttons after absolving/damning.

--- UnityProject/Assets/code/ui/JudgeReportCard.cs
+++ UnityProject/Assets/code/ui/JudgeReportCard.cs
@@ -1,3 +1,4 @@
+using Assets.code.data;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,12 +6,31 @@ namespace Assets.code.ui
 {
   public class JudgeReportCard : MonoBehaviour
   {
+    [SerializeField] Text AbsolvedText = null;
+    [SerializeField] Text DamnedText = null;
     [SerializeField] Text[] reportCardTextColumns = null;
     [SerializeField] JudgeStats judgeStats = null;
     int incrementAmount = 5;
 
     public void OnEnable()
     {
+      int damnedCount = 0;
+      int absolvedCount = 0;
+      foreach(JudgedPerson person in judgeStats.JudgedPeople)
+      {
+        if(person.Damned)
+        {
+          damnedCount++;
+        }
+        else
+        {
+          absolvedCount++;
+        }
+      }
+
+      DamnedText.text = string.Format("Damned Souls: {0}", damnedCount);
+      AbsolvedText.text = string.Format("Absolved Souls: {0}", absolvedCount);
+
       judgeStats.CompileStats();
       for (int i = 0; i < reportCardTextColumns.Length; i++)
       {
