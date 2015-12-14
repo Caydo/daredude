53d74c0732b60a1da0af296aa59d98d9321df6ad
Recursiveanomaly <marnelltom@gmail.com>
2015-12-13 16:54:03 -0800

    Add screech for tires when cars get near each other.

---
+++ UnityProject/Assets/code/TrafficCop2_ElectricBoogaloo/TiresScreech.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class TiresScreech : MonoBehaviour
+{
+    public AudioSource m_audioSource;
+    public Vehicle m_vechilce;
+    
+    void OnTriggerEnter(Collider other)
+    {
+        var otherTiresScreech = other.gameObject.GetComponent<TiresScreech>();
+        if (otherTiresScreech != null) // ignore collision with other things than vehicles
+        {
+            var otherStateMachine = otherTiresScreech.m_vechilce.transform.Cast<Transform>()
+                .Select(t => t.GetComponent<StateMachine>())
+                .Where(t => t != null)
+                .First();
+            if (otherStateMachine.CurrentState == "Go")
+            {
+                m_audioSource.Play();
+            }
+        }
+    }
+}
